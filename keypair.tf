817c8564e9bb5a512a345a5675b310987da690eb
AnastasiyaOhal <59424429+AnastasiyaOhal@users.noreply.github.com>
2024-01-18 19:10:51 -0600

    Update keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -1,5 +1,5 @@
 
 resource "aws_key_pair" "Anastasiya" {
-  key_name   = "access key"
+  key_name   = "new access key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCdF7tiWkgr8oN0gUxXqYtgcjCx5y/CCO8CIcr81Q1NMZ6xKM+cBuMrDE+j6kLoMiDMz7Wrh2CP14F3Y/Gs0cXZ6Uzrv1eCIjdGADzt48F0XgYeURt/Ie0tGBmss2gq1Gdab7ae00GV1nhhOX50oPxXXSapkwQZ9WnJx2vPTSKB7HwtpyTDKmoUcxKlaiSsmiCJ+WHNvutcBHwG66HyZHvujQlNQlhowuWLbkJwLdbh+BTO21YNv2vUQ9ZCPx+bGWE4c8iHkHYCWkZLtuP9eMwwltwUe7LBnCPmf1RrjAjRJon7Vm6BxaLmshau6i22xSTxEtjkVp28TGJC0eIh1Lgl7aci7DjCXt8BAEkrX2Ywe6nYKe/zY/1BObXoERynXkoerkaLBaLIQaMXS506JurqeDSsz8PDGFtKJeJ54eB0Te+ibpCIRV9S0Oqb6qWM5NCmu7ta1E9bIrqXibrvkzlh87nIxYSZUf3bEjTw7QWAGPmU/BqCqz+4v1RxTZ3StKM= nastiaohal@Nastias-MacBook-Pro.local"
 }
